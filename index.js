093487ca0d3f2e3ec58f199160231d48b2994c3f
mireille <mimimireille95@gmail.com>
2019-07-19 11:25:15 -0400

    all about arrays and function

--- index.js
+++ index.js
@@ -1,92 +1,135 @@
-function gName(){
-  var input = document.getElementById('dob').value;
+// function gName(){
+//   var input = document.getElementById('dob').value;
   
-    var day= new Date(input);
+//     var day= new Date(input);
 
-        MM=day.getMonth();
-        MM= MM + 1;
-        DD=day.getDate(); 
-        DD= DD +1;
-        YY=day.getFullYear();
-        // var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];  
-        // var male = ['Kwasi' ,'Kwadwo' , 'Kwabena' ,'Kwako' ,'Yaw' , 'Kofi','Kwamo'];
-        // var female =['Akosua','Adwoa','Abena','Akwa','Yaa','Afua','Ama'];
-    var gender= document.getElementById('gender').value;
+//         MM = day.getMonth();
+//         MM = MM + 1;
+//         DD = day.getDate(); 
+//         DD = DD +1;
+//         YY = day.getFullYear();
 
-    var CCC = (YY - 1) / 100 + 1;
-    var CC = parseFloat(CCC);
-    var d = parseInt(( ((CC/4) - 2*CC -1) +((5* YY / 4)) + ((26*(MM + 1) / 10)) + DD)) % 7;
-    var dd = (d.toFixed(0));
-    
-    document.getElementById('output').innerHTML = output;
-    
-    if (dd == 0 && gender =='male')
+
+const nameDisplay = document.querySelector('#gender');
+const dobInput = document.querySelector('#dob');
+function generate(){
+    let dob = dobInput.value; //to hold value from input
+    var dobArray = dob.split('-'); //makes entered date into an array
+
+    if(dobArray.length !== 3)
     {
+        alert("invalid Date");
+    }
+   else if(!dobArray[0].match(/^\d\d\d\d$/) || !dobArray[1].match(/^\d\d$/) ||  !dobArray[2].match(/^\d\d$/))
+        {
+         alert("invalid Date");    
+        }
+   else if(!dobArray[0].match(/^\d\d\d\d$/) || !dobArray[1].match(/^\d\d$/) || !dobArray[2].match(/^\d\d$/))
+       {
+        alert("invalid Date");    
+       }
+       else{
+          var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];  
+          var male = ['Kwasi' ,'Kwadwo' , 'Kwabena' ,'Kwako' ,'Yaw' , 'Kofi','Kwamo'];
+          var female =['Akosua','Adwoa','Abena','Akwa','Yaa','Afua','Ama'];
+          var currentTime = new Date(parseInt(dobArray[0]),parseInt(dobArray[1]) - 1, //month starts from 0
+            parseInt(dobArray[2])
+        );        
+        var currentDay = currentTime.getDay();       
+        var currentDayName= days[currentDay];
+        var dayName = names[currentDay];
+       nameDisplay.innerHTML = "You were born on "+currentDayName+ " your name is "+ dayName;
+    }
+ 
+ 
+
+
+
+
+
+//  for(i=0; i<7; i++)
+//      {
+//          if(dd == 0 && gender == "male")
+//          {
+
+//          }
+//      }
+    
+//     var gender= document.getElementById('gender').value;
+//     var CCC = (YY - 1) / 100 + 1;
+//     var CC = parseFloat(CCC);
+//     var d = parseInt(( ((CC/4) - 2*CC -1) +((5* YY / 4)) + ((26*(MM + 1) / 10)) + DD)) % 7;
+//     var dd = (d.toFixed(0));
+    
+//     document.getElementById('output').innerHTML = output;
+    
+//     if (dd == 0 && gender =='male')
+//     {
        
-        document.getElementById("output").innerHTML = "Kwasi";
-    } 
+//         document.getElementById("output").innerHTML = "Kwasi";
+//     } 
 
-    else if(dd == 0 && gender == 'female')
-    {
-        document.getElementById("output").innerHTML = "Akosua" ;
+//     else if(dd == 0 && gender == 'female')
+//     {
+//         document.getElementById("output").innerHTML = "Akosua" ;
     
-    }
-    else if (dd == 1 && gender == 'male')
-    {
-        document.getElementById("output").innerHTML = "Kwadwo";
+//     }
+//     else if (dd == 1 && gender == 'male')
+//     {
+//         document.getElementById("output").innerHTML = "Kwadwo";
     
-    } 
-    else if(dd == 1 && gender == 'female')
-    {
-        document.getElementById("output").innerHTML = "Adwoa";
+//     } 
+//     else if(dd == 1 && gender == 'female')
+//     {
+//         document.getElementById("output").innerHTML = "Adwoa";
     
-    }
-    else if (dd == 2 && gender == 'male')
-    {
-        document.getElementById("output").innerHTML = "Kwabena" ;
+//     }
+//     else if (dd == 2 && gender == 'male')
+//     {
+//         document.getElementById("output").innerHTML = "Kwabena" ;
    
-    } 
-    else if(dd == 2 && gender == 'female')
-    {
-        document.getElementById("output").innerHTML = "Abena";
+//     } 
+//     else if(dd == 2 && gender == 'female')
+//     {
+//         document.getElementById("output").innerHTML = "Abena";
     
-    }
-    else if (dd == 3 && gender == 'male')
-    {
-        document.getElementById("output").innerHTML = "Kwako";
+//     }
+//     else if (dd == 3 && gender == 'male')
+//     {
+//         document.getElementById("output").innerHTML = "Kwako";
     
-    } 
-    else if(dd == 3 && gender == 'female')
-    {
-        document.getElementById("output").innerHTML = "Akwa"; 
+//     } 
+//     else if(dd == 3 && gender == 'female')
+//     {
+//         document.getElementById("output").innerHTML = "Akwa"; 
     
-    }
-    else if (d == 4 && gender == 'male')
-    {
-        document.getElementById("output").innerHTML = "Yaw";  
+//     }
+//     else if (d == 4 && gender == 'male')
+//     {
+//         document.getElementById("output").innerHTML = "Yaw";  
     
-    } 
-    else if(d == 4 && gender == 'female')
-    {
-        document.getElementById("output").innerHTML = "Yaa";
+//     } 
+//     else if(d == 4 && gender == 'female')
+//     {
+//         document.getElementById("output").innerHTML = "Yaa";
    
-    }
-   else if (dd == 5 && gender == 'male')
-   {
-    document.getElementById("output").innerHTML = "Kofi";
-   } 
-   else if(dd == 5 && gender == 'female')
-   {
-    document.getElementById("output").innerHTML = "Afua";
-  }
-   else if (dd == 6 && gender == 'male')
-   {
-    document.getElementById("output").innerHTML = "Kwamo";
-   } 
-   else if(dd == 6 && gender == 'female')
-   {
-    document.getElementById("output").innerHTML = "Ama";
-  }
+//     }
+//    else if (dd == 5 && gender == 'male')
+//    {
+//     document.getElementById("output").innerHTML = "Kofi";
+//    } 
+//    else if(dd == 5 && gender == 'female')
+//    {
+//     document.getElementById("output").innerHTML = "Afua";
+//   }
+//    else if (dd == 6 && gender == 'male')
+//    {
+//     document.getElementById("output").innerHTML = "Kwamo";
+//    } 
+//    else if(dd == 6 && gender == 'female')
+//    {
+//     document.getElementById("output").innerHTML = "Ama";
+//   }
   }
 
 
@@ -103,29 +146,7 @@ function gName(){
 // function generate(){
 //  let bday = dobInput.value; //to hold value from input
 
-// var bdayArray = bday.split('-'); //makes entered date into an array
-// if(bdayArray.length !== 3){
-//     alert("invalid Date");
-// }else{
-//    if(!bdayArray[0].match(/^\d\d\d\d$/) || 
-//       !bdayArray[1].match(/^\d\d$/) || 
-//       !bdayArray[2].match(/^\d\d$/)){
-//        alert("invalid Date");    
-//       }else{
-//        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'];  
-//        var names = ['John' ,'Bien' , 'Meghan' ,'Mugisha' ,'Mireille' , 'Fanny'];   
-//        var currentTime = new Date(            
-//            parseInt(bdayArray[0]),
-//            parseInt(bdayArray[1]) - 1, //month starts from 0
-//            parseInt(bdayArray[2])
-//        );        
-//        var currentDay = currentTime.getDay();       
-//        var currentDayName= days[currentDay];
-//        var dayName = names[currentDay];
-//       nameDisplay.innerHTML = "You were born on "+currentDayName+ " your name is "+ dayName;
-//    }
-// }};
-
+// 
 
 
 
